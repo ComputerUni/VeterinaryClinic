48b0cf387ebd443eb5a2dab7a0047e199bcc4e45
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-06-03 00:02:47 +0300

    Hayvan Listesi oluşturuldu frontend'de düzenlemeler yapıldı

--- VeterinaryClinic/VeterinaryClinic.UI/obj/Debug/net8.0/VeterinaryClinic.UI.AssemblyInfo.cs
+++ VeterinaryClinic/VeterinaryClinic.UI/obj/Debug/net8.0/VeterinaryClinic.UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeterinaryClinic.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72ad4e473bd7167d29ccb25c195c4d5bfe02bf58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7230ecb3d0e54ce7ddd98724cbb8efb65a867795")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeterinaryClinic.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeterinaryClinic.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
