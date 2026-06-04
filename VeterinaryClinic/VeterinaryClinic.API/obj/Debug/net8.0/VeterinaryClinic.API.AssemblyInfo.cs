8533293e4b63878bdaf57a1367cc7bad130a3132
Mehmet Kadir Güngör <m.kadir-27@hotmail.com>
2026-06-05 00:55:52 +0300

    API Key düzenlendi

--- VeterinaryClinic/VeterinaryClinic.API/obj/Debug/net8.0/VeterinaryClinic.API.AssemblyInfo.cs
+++ VeterinaryClinic/VeterinaryClinic.API/obj/Debug/net8.0/VeterinaryClinic.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeterinaryClinic.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e617c1d9090414573b96d18e55071cce9f3c5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0543774e5527d854a66accf952be70b5625984")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeterinaryClinic.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeterinaryClinic.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
